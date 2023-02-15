ffb34dddbc01b505b70c555d2f475d4ecec3248f
eddieavd <edhemavdagic@gmail.com>
2023-02-15 13:50:36 +0100

    Initial Commit

---
+++ hush/hushApp.swift
@@ -0,0 +1,17 @@
+//
+//  hushApp.swift
+//  hush
+//
+//  Created by Edhem Avdagic on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct hushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
